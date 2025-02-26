d5728cb0f7e93464bcd7f7a3a0faa0943b6bfd0c
Amanda McDermott <amanda.mcdermott@coinbase.com>
2025-02-26 10:55:09 -0500

    Correct env var

--- site/next.config.ts
+++ site/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    FACILITATOR_WALLET_ADDRESS: process.env.FACILITATOR_WALLET_ADDRESS,
+    RESOURCE_WALLET_ADDRESS: process.env.RESOURCE_WALLET_ADDRESS,
     NEXT_PUBLIC_FACILITATOR_URL: process.env.NEXT_PUBLIC_FACILITATOR_URL,
   },
 };
