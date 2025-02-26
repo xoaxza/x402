ed29ffa901415f16fade58ec4db3670369a3ff89
Amanda McDermott <amanda.mcdermott@coinbase.com>
2025-02-26 02:06:32 -0500

    swap out for resource wallet address

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const config: NextConfig = {
+const nextConfig: NextConfig = {
   env: {
     FACILITATOR_WALLET_ADDRESS: process.env.FACILITATOR_WALLET_ADDRESS,
     NEXT_PUBLIC_FACILITATOR_URL: process.env.NEXT_PUBLIC_FACILITATOR_URL,
   },
 };
 
-export default config;
+export default nextConfig;
