c3dbb588b49c9aa151289b7fe96b620676ff7c9f
Carson Roscoe <CarsonRoscoe7@gmail.com>
2025-04-25 14:02:33 -0400

    fix: missing env vars in next config (#80)

--- typescript/site/next.config.ts
+++ typescript/site/next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   env: {
     RESOURCE_WALLET_ADDRESS: process.env.RESOURCE_WALLET_ADDRESS,
     NEXT_PUBLIC_FACILITATOR_URL: process.env.NEXT_PUBLIC_FACILITATOR_URL,
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
+    NETWORK: process.env.NETWORK,
   },
   webpack(config) {
     config.module.rules.push({
