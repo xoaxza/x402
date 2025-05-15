36bdb9589927e6641da0408671d0fc0520d1891f
Carson Roscoe <CarsonRoscoe7@gmail.com>
2025-05-15 17:25:54 -0400

    release: bump package versions (#179)

--- typescript/packages/coinbase-x402/src/index.ts
+++ typescript/packages/coinbase-x402/src/index.ts
@@ -5,7 +5,7 @@ import { CreateHeaders } from "x402/verify";
 const COINBASE_FACILITATOR_BASE_URL = "https://api.cdp.coinbase.com";
 const COINBASE_FACILITATOR_V2_ROUTE = "/platform/v2/x402";
 
-const X402_VERSION = "0.3.4";
+const X402_VERSION = "0.3.5";
 const SDK_VERSION = "1.1.1";
 
 /**
